1473332ecdb4f26794960b73e29bc284a5ae33f9
jiun <vonreal20@gmail.com>
2022-07-11 15:48:08 +0900

    🐛 UI 버그 수정 - 버튼이 레이블을 가리는 버그 수정 레이블을 버튼과 equal height으로 비율 설정해서 해결

---
+++ sesac-week2/emotionDiaryViewController.swift
@@ -0,0 +1,27 @@
+//
+//  emotionDiaryViewController.swift
+//  sesac-week2
+//
+//  Created by 나지운 on 2022/07/11.
+//
+
+import UIKit
+
+class emotionDiaryViewController: UIViewController {
+
+//    @IBOutlet weak var listButton: UIButton!
+//
+//    @IBOutlet weak var titleLabel: UILabel!
+//    var happy_idx = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        designTitle()
+    }
+    
+//    func designTitle() {
+//        listButton.tintColor = .darkGray
+//        titleLabel.text = "감정 다이어리"
+//    }
+}

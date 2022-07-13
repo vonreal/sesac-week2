afcadb7c1fa2ad77032f526c6784538a0747e859
jiun <vonreal20@gmail.com>
2022-07-14 08:50:45 +0900

    ➕ [기념일 계산기 UI] 스토리보드 기본 UI 구현 파일 추가

---
+++ sesac-week2/AnniversaryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AnniversaryViewController.swift
+//  sesac-week2
+//
+//  Created by 나지운 on 2022/07/14.
+//
+
+import UIKit
+
+class AnniversaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}

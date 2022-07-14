f4cac7bb85e5ff37fa0749d58411063b2abdb4f4
jiun <vonreal20@gmail.com>
2022-07-14 12:15:21 +0900

    ✅ [기념일 캘린더 UI] UI 코드 추가

--- sesac-week2/AnniversaryViewController.swift
+++ sesac-week2/AnniversaryViewController.swift
@@ -9,8 +9,21 @@ import UIKit
 
 class AnniversaryViewController: UIViewController {
 
+    @IBOutlet var imageViewList: [UIImageView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+
+        
+        for image in imageViewList {
+            let tintView = UIView()
+            
+            tintView.backgroundColor = UIColor(white: 0, alpha: 0.2)
+            tintView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+            image.layer.cornerRadius = 10
+            image.addSubview(tintView)
+        }
     }
 
 }
